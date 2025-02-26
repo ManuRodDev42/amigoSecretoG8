c6d6a97826c42088d6ac317c9d032c69ecb3129f
ManuRodDev42 <manuelalejandrorod4220@gmail.com>
2025-02-25 23:54:38 -0500

    resultado del proyecto

---
+++ app.js
@@ -0,0 +1,57 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+function agregarAmigos() {
+    const inputAmigo = document.getElementById("amigo");
+    const nombreAmigo = inputAmigo.value.trim();
+
+    if (nombreAmigo === "") {
+        alert("Debe contener un nombre válido");
+        return;
+    }
+
+    listaAmigos.push(nombreAmigo);
+
+    actualizacionListaAmigo();
+
+    inputAmigo.value = "";
+    inputAmigo.focus();
+}
+
+function actualizacionListaAmigo() {
+    const listaAmigosUl = document.getElementById("listaAmigos")
+    listaAmigosUl.innerHTML = "";
+
+    listaAmigos.forEach(amigo => {
+        const li = document.createElement("li");
+        li.textContent = amigo;
+        listaAmigosUl.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if(listaAmigos.length === 0){
+        alert("No hay amigos para sortear. Agrega nombres primero.");
+        return
+    }
+
+    const indiceAleatorio = Math.floor(Math.random()*listaAmigos.length);
+    const amigoSorteado = listaAmigos[indiceAleatorio];
+    const resusltadoUl = document.getElementById("resultado");
+
+    resusltadoUl.innerHTML = `<li>${amigoSorteado}</li>`;
+}
+
+function nuevoSorteo() {
+    listaAmigos = [];
+    const listaAmigosUl = document.getElementById("listaAmigos");
+    listaAmigosUl.innerHTML = "";
+    const resultadoUl = document.getElementById("resultado");
+    resultadoUl.innerHTML = "";
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById("btnAdicionar").addEventListener("click", agregarAmigos);
+    document.getElementById("btnSortear").addEventListener("click", sortearAmigo);
+    document.getElementById("btnNuevoSorteo").addEventListener("click", nuevoSorteo);
+});
